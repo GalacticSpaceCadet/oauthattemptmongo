2c82c3c1e9d52fcb15c53c1899fb43a54f6e080c
Di Gandy <digandy@Dis-MacBook-Pro.lan>
2017-05-03 22:53:55 -0500

    removed redundant server init

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,8 +11,6 @@ router.get('/auth/linkedin/callback',
     res.json(req.user);
   });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+
 
   var passportLinkedIn = require('../auth/linkedin');
